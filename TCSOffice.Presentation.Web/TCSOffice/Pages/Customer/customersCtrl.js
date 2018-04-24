00a6188976e414afb3f77860745d0ae93f6ad139
Shefali Kapoor <kapoorshefali2@gmail.com>
2018-04-24 23:04:15 +0530

    remove errors

---
+++ TCSOffice.Presentation.Web/TCSOffice/Pages/Customer/customersCtrl.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'customersCtrl';
+    App.controller(controllerId, ['$scope', '$routeParams', customersCtrl]);
+    function customersCtrl($scope, $routeParams) {
+        $scope.test = "gvvdchjbdshchjdsb";
+    }
+})();
